04c26b7e940a18984d49c181b3e12d5b7b8c8bb1
deepanm87 <deepan.murugan29@gmail.com>
2025-05-12 20:48:11 -0400

    add to cart and calculate cart total functionality

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,12 +1,20 @@
-import "./cart-dropdown.styles.scss"
+import { useContext } from "react"
 import Button from "../button/button"
+import CartItem from "../cart-item/cart-item"
+import { CartContext } from "../../contexts/cart.context"
+import "./cart-dropdown.styles.scss"
 
 const CartDropdown = () => {
+    const { cartItems } = useContext(CartContext)
+    
     return(
         <div className="cart-dropdown-container">
             <div className="cart-items">
-                <Button>Checkout</Button>
+                { cartItems.map( item => (
+                    <CartItem key={item.key} cartItem={item} />
+                ))}
             </div>
+            <Button>Checkout</Button>
         </div>
     )
 }
