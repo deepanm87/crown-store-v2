95f7694695e2068a78aa71b69c03ba3bf74eefc9
deepanm87 <deepan.murugan29@gmail.com>
2025-05-13 20:31:34 -0400

    added category preview and category components

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,24 +1,15 @@
-import { useContext, Fragment } from "react"
-import { CategoriesContext } from "../../contexts/categories.context"
-import ProductCard from "../../components/product-card/product-card"
+import { Routes, Route } from "react-router-dom"
+import CategoriesPreview from "../categories-preview/categories-preview"
+import Category from "../category/category"
 import "./shop.styles.css"
 
 const shop = () => {
-    const { categoriesMap }  = useContext(CategoriesContext)
-
+ 
     return(
-        <Fragment>
-            { Object.keys(categoriesMap).map( title => (
-                <Fragment key={title}>
-                    <h2>{title}</h2>
-                    <div className="products-container">
-                        { categoriesMap[title].map( product => (
-                            <ProductCard key={product.id} product={product} />
-                        ))}
-                    </div>
-                </Fragment>
-            ))}
-        </Fragment>
+        <Routes>
+            <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
+        </Routes>
     )
 }
 
