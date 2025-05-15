6a28fc9864343914850c5e53d72d2970103d3a13
deepanm87 <deepan.murugan29@gmail.com>
2025-05-14 21:24:51 -0400

    fixed scss bug

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import CategoriesPreview from "../categories-preview/categories-preview"
 import Category from "../category/category"
-import "./shop.styles.css"
+import "./shop.styles.scss"
 
 const shop = () => {
  
