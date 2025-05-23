c037126e41af39c7e09af9638960a622199e7252
deepanm87 <deepan.murugan29@gmail.com>
2025-05-22 20:38:32 -0400

    added ts functionality for middleware, user, categories

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -7,9 +7,9 @@ export type FetchCategoriesSuccess = ActionWithPayload<CATEGORIES_ACTION_TYPES.F
 
 export type FetchCategoriesFailed = ActionWithPayload<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, Error>
 
-export const fetchCategoriesStart = withMatcher(() => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START))
+export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START))
 
-export const fetchCategoriesSuccess = withMatcher((categoriesArray: Category[]) => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray))
+export const fetchCategoriesSuccess = withMatcher((categoriesArray: Category[]): FetchCategoriesSuccess => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray))
 
-export const fetchCategoriesFailed = withMatcher((error: Error) => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error))
+export const fetchCategoriesFailed = withMatcher((error: Error): FetchCategoriesFailed => createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error))
 
