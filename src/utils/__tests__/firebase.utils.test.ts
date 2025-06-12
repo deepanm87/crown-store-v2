500f0f0aa9c0d0a36d8dab89d35322afcdd615a9
deepanm87 <deepan.murugan29@gmail.com>
2025-06-12 14:55:23 -0400

    updated tests

--- src/utils/__tests__/firebase.utils.test.ts
+++ src/utils/__tests__/firebase.utils.test.ts
@@ -7,4 +7,54 @@ import {
     signOutUser 
 } from "../firebase/firebase.utils"
 
+jest.mock('firebase/auth', () => {
+    return {
+        getAuth: jest.fn(),
+        GoogleAuthProvider: jest.fn().mockImplementation(() => {
+            return {
+                setCustomParameters: jest.fn()
+            }
+    }),
+    signInWithPopup: jest.fn(),
+    signInWithRedirect: jest.fn(),
+    signInWithEmailAndPassword: jest.fn(),
+    createUserWithEmailAndPassword: jest.fn(),
+    onAuthStateChanged: jest.fn(),
+    signOut: jest.fn()
+    }
+})
+
+describe('firebase utils', () => {
+    test("signInWithGooglePopup to call firestoreAuth's signInWithPopup", () => {
+        signInWithGooglePopup()
+        expect(firestoreAuth.signInWithPopup).toHaveBeenCalled()
+    })
+
+    test("signInWithGoogleRedirect to call firestoreAuth's signInWithRedirect", () => {
+        signInWithGoogleRedirect()
+        expect(firestoreAuth.signInWithRedirect).toHaveBeenCalled()
+    })
+
+    test("signInAuthUserWithEmailAndPassword to call firestoreAuth's signInWithEmailAndPassword", () => {
+        const testEmail = 'testEmail'
+        const testPassword = 'testPassword'
+        signInAuthUserWithEmailAndPassword(testEmail, testPassword)
+
+        expect(firestoreAuth.signInWithEmailAndPassword).toHaveBeenCalledWith(undefined, testEmail, testPassword)
+    })
+
+    test("createAuthUserWithEmailAndPassword to call firestoreAuth's createUserWithEmailAndPassword", () => {
+        const testEmail = 'testEmail'
+        const testPassword = 'testPassword'
+        createAuthUserWithEmailAndPassword(testEmail, testPassword)
+
+        expect(firestoreAuth.createUserWithEmailAndPassword).toHaveBeenCalledWith(undefined, testEmail, testPassword)
+    })
+
+    test("signOutUser is to call firestoreAuth's signOut", () => {
+        signOutUser()
+        expect(firestoreAuth.signOut).toHaveBeenCalled()
+    })
+})
+
 
